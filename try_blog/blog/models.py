cbb8458c856299b79c0807b7e419bd7a321136ce
fire <fire@fire-ubuntu19.04-desktop>
2019-09-29 16:41:05 +0800

    完成了 blog, comment, config 三个 APP 的 admin.py

--- try_blog/blog/models.py
+++ try_blog/blog/models.py
@@ -17,7 +17,10 @@ class Category(models.Model):
         verbose_name='状态')
     is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
-    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = verbose_name_plural = '分类'
@@ -37,7 +40,10 @@ class Tag(models.Model):
         choices=STATUS_ITEMS,
         verbose_name='状态')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
-    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
@@ -62,6 +68,9 @@ class Post(models.Model):
     tag = models.ManyToManyField(Tag, verbose_name='标签')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = verbose_name_plural = '文章'
